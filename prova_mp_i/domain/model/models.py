47c284d8b093f32b8e631177787eec1450f0a7e6
Leao10 <gabriellfrigo@gmail.com>
2024-07-06 17:42:56 -0300

    aula 03

--- prova_mp_i/domain/model/models.py
+++ prova_mp_i/domain/model/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import Session, relationship
+from sqlalchemy import Column, Integer, String
 
 from prova_mp_i.config.database import Base, engine
 
@@ -9,65 +8,13 @@ class Usuario(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(50))
-    email = Column(String(50))
+    email = Column(String(50), unique=True)
     password = Column(String(50))
-    cpf = Column(String(11))
+    cpf = Column(String(11), unique=True)
     phone = Column(String(11))
 
-    endereco_id = Column(Integer, ForeignKey('enderecos.id', ondelete='CASCADE'))
-    endereco = relationship('Endereco', back_populates='usuario', uselist=False)
-
-    
-
     def __repr__(self):
-        return f'<User(id={self.id}, name={self.name}, email={self.email}, password={self.password}, cpf={self.cpf}, phone={self.phone}, endereco={self.endereco})>'
-
-
-class Endereco(Base):
-    __tablename__ = 'enderecos'
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    cep = Column(String(8))
-    logradouro = Column(String(100))
-    numero = Column(String(10))
-    complemento = Column(String(50))
-    bairro = Column(String(50))
-    cidade = Column((String(50)))
-    estado = Column(String(2))
-    usuario = relationship('Usuario', back_populates='endereco', uselist=False)
-
-    @staticmethod
-    def create(session: Session, **kwargs):
-        endereco = Endereco(**kwargs)
-        session.add(endereco)
-        session.commit()
-        session.refresh(endereco)
-        return endereco
-
-    @staticmethod
-    def update(session: Session, endereco_id: int, **kwargs):
-        endereco = Endereco.read(session, endereco_id)
-        if endereco:
-            for key, value in kwargs.items():
-                setattr(endereco, key, value)
-            session.commit()
-            session.refresh(endereco)
-        return endereco
-
-    @staticmethod
-    def delete(session: Session, user_id: int):
-        endereco = Endereco.read(session, user_id)
-        if endereco:
-            session.delete(endereco)
-            session.commit()
-        return user_id
-
-    @staticmethod
-    def read(session: Session, user_id: int):
-        return session.query(Endereco).filter(Endereco.id == user_id).first()
-
-    def __repr__(self):
-        return f'<Endereco(id={self.id}, cep={self.cep}, logradouro={self.logradouro}, numero={self.numero}, complemento={self.complemento}, bairro={self.bairro}, cidade={self.cidade}, estado={self.estado})>'
-
+        return f'<User(id={self.id}, name={self.name}, email={self.email}, password={self.password}, cpf={self.cpf}, phone={self.phone})>'
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
--- prova_mp_i/repository/usuario_repository.py
+++ prova_mp_i/repository/usuario_repository.py
@@ -2,49 +2,23 @@ from sqlalchemy.orm import Session
 
 from prova_mp_i.domain.model.models import Usuario
 
-class IUserRepository():
-    def create(self, user: object):
-        raise NotImplementedError
-    
-    def read(self, user_id: int):
-        raise NotImplementedError
-    
-    def update(self, user: object, user_data: dict):
-        raise NotImplementedError
-    
-    def delete(self, user: object):
-        raise NotImplementedError
-    
-    def find_all(self):
-        raise NotImplementedError
+class UsuarioRepository:
 
-class UsuarioRepository(IUserRepository):
-
-    def __init__(self, session:Session):
+    def __init__(self, session: Session):
         self.session = session
 
-
-    def create(self, user = Usuario):
+    def save(self, user: Usuario):
         self.session.add(user)
         self.session.commit()
         self.session.refresh(user)
         return user
 
-    def update(self, user_id: int, **kwargs):
-        user = self.read(user_id=user_id)
-        if user:
-            for key, value in kwargs.items():
-                setattr(user, key, value)
-            self.session.commit()
-            self.session.refresh(user)
-        return user
-
-    def delete(self, user_id: int):
-        user = self.read(user_id=user_id)
-        if user:
-            self.session.delete(user)
-            self.session.commit()
-        return user_id
+    def delete(self, user: Usuario):
+        self.session.delete(user)
+        self.session.commit()
 
     def read(self, user_id: int):
         return self.session.query(Usuario).filter(Usuario.id == user_id).first()
+
+    def find_all(self):
+        return self.session.query(Usuario).all()
